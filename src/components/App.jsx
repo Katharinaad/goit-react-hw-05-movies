2601cc6c33796775776da88968234de91ceb9634
Katharinaad <vermilion.adams@gmail.com>
2023-11-14 23:13:38 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,14 @@
-export const App = () => {};
+// import { NavLink } from 'react-router-dom';
+
+import { Navbar, StyledLink } from './App.styled';
+
+export const App = () => {
+  return (
+    <header>
+      <Navbar>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/movies">Movies</StyledLink>
+      </Navbar>
+    </header>
+  );
+};
