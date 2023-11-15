62d122db1b305b55f76713e4606550c6d1974763
Katharinaad <vermilion.adams@gmail.com>
2023-11-16 00:02:05 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,29 @@
-// import { NavLink } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
 
 import { Navbar, StyledLink } from './App.styled';
+import Loader from './Loader';
+
+import HomePage from 'pages/HomePage';
 
 export const App = () => {
   return (
-    <header>
-      <Navbar>
-        <StyledLink to="/">Home</StyledLink>
-        <StyledLink to="/movies">Movies</StyledLink>
-      </Navbar>
-    </header>
+    <>
+      <header>
+        <Navbar>
+          <StyledLink to="/">Home</StyledLink>
+          <StyledLink to="/movies">Movies</StyledLink>
+        </Navbar>
+      </header>
+
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" />
+          <Route index element={<HomePage />} />
+          <Route path="/search" />
+          <Route path="/post-details/:postId/*" />
+        </Routes>
+      </Suspense>
+    </>
   );
 };
