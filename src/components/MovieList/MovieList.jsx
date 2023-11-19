3f756668e474a35416838d972d9bb09453c1cbb4
Katharinaad <vermilion.adams@gmail.com>
2023-11-19 11:45:28 +0100

    Create MovieList.jsx

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,20 @@
+import { Link, useLocation } from 'react-router-dom';
+import './MovieList.scss';
+
+const MovieList = ({ movies }) => {
+  const location = useLocation();
+
+  return (
+    <ul className="movie-list">
+      {movies.map(({ id, title }) => (
+        <li key={id}>
+          <Link to={`/movies/${id}`} state={{ from: location }}>
+            {title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
