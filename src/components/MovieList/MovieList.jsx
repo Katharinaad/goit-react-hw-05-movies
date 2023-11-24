095f0d99da9b8d6182c67d4e9ad7c22366a43ace
Katharinaad <vermilion.adams@gmail.com>
2023-11-24 19:08:23 +0100

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -8,9 +8,7 @@ const MovieList = ({ movies }) => {
     <ul className="movie-list">
       {movies.map(({ id, title }) => (
         <li key={id}>
-          <Link to={`/movies/${id}`} state={{ from: location }}>
-            {title}
-          </Link>
+          <Link to={`/movies/${id}`}>{title}</Link>
         </li>
       ))}
     </ul>
