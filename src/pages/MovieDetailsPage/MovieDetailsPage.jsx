8769eb182b3fa0050f21072f823ec89a557d0fcd
Katharinaad <vermilion.adams@gmail.com>
2023-11-26 17:16:33 +0100

    adding go back

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,20 +1,27 @@
+import Loader from 'components/Loader/Loader';
 import { MovieDetail } from 'components/MovieDetail/MovieDetail';
-import { useEffect, useState } from 'react';
-import { Outlet, useParams } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 import { fetchMovieDetailed } from 'services/api';
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const location = useLocation();
+  const backLinkHref = useRef(location.state?.from ?? '/movies');
 
   useEffect(() => {
     // fetch movie details
     const fetchMovie = async () => {
       try {
+        setIsLoading(true);
         const data = await fetchMovieDetailed(movieId);
         setMovie(data);
       } catch (error) {
         console.log('Error fetching details for the movie:', error);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchMovie();
@@ -22,6 +29,8 @@ const MovieDetailsPage = () => {
 
   return (
     <>
+      {isLoading && <Loader />}
+      <Link to={backLinkHref.current}>Go Back</Link>
       <MovieDetail movie={movie} />
       <Outlet />
     </>
